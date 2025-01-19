0c76f4037bffb369d44cd3bd3913966c346d7372
dimoha_zadira <DimohaZadira@gmail.com>
2025-01-20 02:00:21 +0300

    fix init migration

--- migrations/0001__initial.sql
+++ migrations/0001__initial.sql
@@ -2,7 +2,7 @@ create schema if not exists english_tutor;
 
 drop table if exists english_tutor.user;
 
-create table english_tutor.user (
+create table english_tutor.et_test_user (
 	id uuid primary key, 
 	phone text, 
 	crm text, 
